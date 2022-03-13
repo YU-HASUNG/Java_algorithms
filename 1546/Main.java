ed7b998ffe9aaf05b7ec2155f8211bcd7145dfde
hasung <dbgktjd@naver.com>
2022-03-13 18:06:33 +0900

    백준 1546 1차원배열

---
+++ 1546/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int A = Integer.parseInt(br.readLine());
+        double Max = 0;
+        double Sum = 0;
+        double[] array = new double[A];
+
+        StringTokenizer st = null;
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<A;i++){
+            array[i] = Integer.parseInt(st.nextToken());
+            if(Max <= array[i]){
+                Max = array[i];
+            }
+        }
+
+        for(int i=0;i<A;i++){
+            array[i] = (array[i]/Max)*100;
+        }
+
+        for(int i=0;i<A;i++){
+            Sum = Sum + array[i];
+        }
+
+        System.out.println(Sum/A);
+    }
+}
